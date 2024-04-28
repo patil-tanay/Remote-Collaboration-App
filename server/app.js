aac5325cd262e42a55e630b22a1413344147ce77
somtyagi20 <somnathtyagi210787@acropolis.in>
2024-04-28 07:42:05 +0530

    Add server setup code and routes in index.js and app.js [ routes -> register user and login user ]

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//Routes
+import userRoute from "./Routes/userRoute.js";
+
+//Routes Declaration
+app.use("/api/v1/users", userRoute);
+
+export default app;
