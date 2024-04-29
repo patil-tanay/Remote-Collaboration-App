470cea07833c6356cbae3db5a4373f946ab624b6
somtyagi20 <somnathtyagi210787@acropolis.in>
2024-04-29 06:24:18 +0530

    Refactor package-lock.json, chatController.js, messageController.js, chatRoutes.js, and messageRoutes.js

--- server/Routes/chatRoutes.js
+++ server/Routes/chatRoutes.js
@@ -1,6 +1,6 @@
-import express, { Router } from "express";
+import { Router } from "express";
 import { verifyJWT } from "../Middlewares/auth.js";
-import { upload } from "../Middlewares/multer.js";
+
 import {
   accessChat,
   fetchChat,
@@ -8,8 +8,8 @@ import {
   renameGroup,
   addToGroup,
   removeFromGroup,
-  sendFiles,
 } from "../Controllers/chatController.js";
+
 const router = Router();
 
 router.route("/").post(verifyJWT, accessChat);
@@ -18,6 +18,5 @@ router.route("/group").post(verifyJWT, createGroupChat);
 router.route("/rename").put(verifyJWT, renameGroup);
 router.route("/groupremove").put(verifyJWT, removeFromGroup);
 router.route("/groupadd").put(verifyJWT, addToGroup);
-router.route("/files").post(upload.single(), verifyJWT, sendFiles);
 
 export default router;
